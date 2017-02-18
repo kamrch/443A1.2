b127e2b1cf54a2ac4ec9770e4d121e6843336d50
Ryan <ryan921ryan@hotmail.com>
2017-02-18 17:53:26 -0500

    WIP part 1.2

--- disk_sort.c
+++ disk_sort.c
@@ -15,8 +15,10 @@
 
 //taken from handout
 int compare (const void *a, const void *b) {
-    int a_f = ((const struct Record*)a)->f;
-    int b_f = ((const struct Record*)b)->f;
+    Record r1 = *(Record*) a;
+    Record r2 = *(Record*) b;
+    int a_f = r1.UID2;
+    int b_f = r2.UID2;
     return (a_f - b_f);
 }
 
@@ -39,4 +41,69 @@ void print_buffer(Record* buffer, int total_records){
     for (int i=0; i<total_records; i++){
         printf("uid2 = %d, uid1 = %d\n", buffer[i].UID2, buffer[i].UID1);
     }
+}
+
+// function for Phase 1 of 2PMMS
+int phase1(char* input_file, int total_mem, int block_size){
+    FILE *fp_read, *fp_write;
+    char *prefix = "sorted_list";
+    int total_records;
+
+
+    //check if enough memory
+    if(block_size > total_mem){
+        exit(0);
+    }
+    int block_num = (total_mem / block_size) / 2;
+    if (block_num < 1){
+        printf("Error: insufficient memory.\n");
+        exit(1);
+    }
+
+    int chunks = block_num * block_size / sizeof(Record);
+
+    if (!(fp_read= fopen (input_file , "r" ))) {
+        printf ("Error when reading file \"%s\"\n", input_file);
+        exit(0);
+    }
+
+
+
+    /*finding the file size and total number of records*/
+    fseek(fp_read, 0, SEEK_END);
+    int file_size = ftell(fp_read);
+    total_records = file_size / sizeof(Record);
+    fseek(fp_read, 0, SEEK_SET);
+    int temp_total = total_records;
+    int i = 0;
+
+    Record * buffer = (Record *) calloc (chunks, sizeof (Record));
+
+    while (temp_total > 0){
+        int read_records = fread(buffer, sizeof(Record), chunks, fp_read);
+        if (read_records < 0) {
+            printf ("Error when reading file \"%s\" \n", input_file);
+            exit(1);
+        } else {
+            // sort list then write to disk
+            sort(buffer, temp_total);
+            //save contents of each iteration into different files.
+            char output_file[MAX_PATH_LENGTH];
+            snprintf(output_file, sizeof(char) * MAX_PATH_LENGTH, "sorted_list%1.dat", i);
+            if (!(fp_write = fopen ( output_file , "wb" ))) {
+                printf ("Error when writing file sorted_list  \n");
+                exit(0);
+            }
+    	    fwrite (buffer, sizeof(Record), read_records, fp_write);
+            fclose(fp_write);
+
+            temp_total -= read_records;
+            i++;
+        }
+    }
+
+    free(buffer);
+    fclose(fp_read);
+
+    return i;
 }
--- main.c
+++ main.c
@@ -5,6 +5,20 @@ int main (int argc, char **argv) {
 
 	MergeManager manager;
 
+	/*Check if user input correct arguments*/
+	if(argc != 4){
+		fprintf(stderr, "Usage : main <input filename> <total memory size> <block size>.\n");
+		return (-1);
+	}
+
+	char *input_file = argv[1];
+	int total_mem = atoi(argv[2]);
+	int block_size = atoi(argv[3]);
+
+
+
+	int num_trunks = phase1(input_file, total_mem, block_size);
+
 	//initialize all fields according to the input and the results of Phase I
-	return merge_runs (&manager);
+	return 0;
 }
